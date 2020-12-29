4d8b4e6e4fa36dd77136dcffafde00a03c899f55
jonathanmorais <jonathan.lucena@zoom.com.br>
2020-12-29 19:28:01 -0300

    finalmente voltei a estudar isso

--- fifa/main.py
+++ fifa/main.py
@@ -1,12 +1,39 @@
-import requests
 import pandas as pd
 import csv
-from io import StringIO
 
-df = pd.read_csv('FullData.csv', sep=',')
+def read_dataframe(csv_path):
 
-rating = df.sort_values(by=['Rating']).apply(lambda x: ','.join(set(str(x).split(','))))
+    dataframe = pd.read_csv(csv_path, sep=',')
 
-nationality = df[['Nationality']] + df.sort_values(by=['Rating'])
+    return dataframe
 
-nationality.to_csv('output.csv')
+def extract_columns():
+    
+    df = read_dataframe(csv_path)
+
+    best_player = df['Name'].head(11)
+
+    nationality = df['Nationality'].head(11)
+
+    rate = df['Rating'].head(11)
+
+    return best_player, nationality, rate
+
+def rate_players():
+
+    best_player, _, _ = extract_columns()
+    _, nationality, _ = extract_columns()
+    _, _, rate = extract_columns()
+
+    frames = [best_player, nationality, rate]
+
+    result = pd.concat(frames, axis=1)
+
+    print(result)
+
+    return result
+
+if __name__ == "__main__":
+    csv_path = 'fifa/FullData.csv'
+    read_dataframe(csv_path)
+    rate_players()
